68c5c95bf770aabc97881cc2368b32475f6df36c
ItsMohi <mohi222@pm.me>
2023-03-06 03:11:45 +0330

    new feathers app

---
+++ test/client.test.ts
@@ -0,0 +1,17 @@
+import assert from 'assert'
+import axios from 'axios'
+import { app } from '../src/app'
+import { createClient } from '../src/client'
+
+import rest from '@feathersjs/rest-client'
+
+const port = app.get('port')
+const appUrl = `http://${app.get('host')}:${port}`
+
+describe('client tests', () => {
+  const client = createClient(rest(appUrl).axios(axios))
+
+  it('initialized the client', () => {
+    assert.ok(client)
+  })
+})
